3f30a1e900df9b8259a763fa5ab9938b43fda56d
Toni Dobrinski <tdobrinski@abv.bg>
2025-11-12 01:26:42 +0200

    fix npm run build error

--- app/api/telescopes/[id]/route.ts
+++ app/api/telescopes/[id]/route.ts
@@ -1,13 +1,21 @@
-export async function GET(req, { params }) {
-  console.log("params.id:", params.id);
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(
+  req: NextRequest,
+  context: { params: Promise<{ id: string }> }
+) {
+  const { id } = await context.params;
+
+  console.log("params.id:", id);
+
   const telescopes = [
     {
       id: 1,
       name: "Hubble Telescope",
-      network: "NASA",
-      location: "Earth Orbit",
       latitude: 28.5,
       longitude: -80.6,
+      network: "NASA",
+      location: "Earth Orbit",
       elevation: 0,
       status: "Active",
       lastObservation: "2025-03-01",
@@ -16,25 +24,11 @@ export async function GET(req, { params }) {
     },
     {
       id: 2,
-      name: "VLT",
-      network: "ESO",
-      location: "Chile",
-      latitude: -24.6,
-      longitude: -70.4,
-      elevation: 2635,
-      status: "Active",
-      lastObservation: "2025-03-05",
-      image: "/vlt.jpg",
-      description:
-        "The Very Large Telescope (VLT) is operated by ESO in Chile.",
-    },
-    {
-      id: 3,
       name: "Keck Observatory",
-      network: "Mauna Kea",
-      location: "Hawaii",
       latitude: 19.8,
       longitude: -155.5,
+      network: "Mauna Kea",
+      location: "Hawaii",
       elevation: 4205,
       status: "Offline",
       lastObservation: "2025-02-15",
@@ -42,12 +36,26 @@ export async function GET(req, { params }) {
       description:
         "The Keck Observatory consists of two large telescopes in Hawaii.",
     },
+    {
+      id: 3,
+      name: "VLT",
+      latitude: -24.6,
+      longitude: -70.4,
+      network: "ESO",
+      location: "Chile",
+      elevation: 2635,
+      status: "Active",
+      lastObservation: "2025-03-05",
+      image: "/vlt.jpg",
+      description:
+        "The Very Large Telescope (VLT) is operated by ESO in Chile.",
+    },
   ];
 
-  const telescope = telescopes.find((t) => t.id === parseInt(params.id, 10));
+  const telescope = telescopes.find((t) => t.id === parseInt(id, 10));
 
   if (telescope) {
-    return Response.json(telescope);
+    return NextResponse.json(telescope);
   } else {
     return new Response("Not Found", { status: 404 });
   }
